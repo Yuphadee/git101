8bab0026516a9a104256c526f85d763380cf6f98
Yuphadee <aomsinyuphadee@gmail.com>
2024-02-03 12:42:29 +0700

    say hello git in C

--- code-01.c
+++ code-01.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    char text[] = "Hello Git";
-    printf("%s\n", text);
-    return 0;
+   char text[] = "Hello Git";
+   printf("%s\n", text);
+   return 0;
 }
